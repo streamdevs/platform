e058e2be87af1f21866f7fdb7bcd3fba582b8baa
Santiago Martín Agra <santiagomartinagra@gmail.com>
2020-11-21 22:03:39 +0100

    add new 'GitHubWebhookCard' component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,58 +1,32 @@
-import {
-	Button,
-	Center,
-	Input,
-	InputGroup,
-	InputRightElement,
-	Stack,
-	Text,
-	useClipboard,
-} from '@chakra-ui/react';
+import { Stack } from '@chakra-ui/react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useUser } from 'reactfire';
 
+import { GitHubWebhookCard } from '../components/GitHubWebhookCard';
 import { Layout } from '../components/Layout';
 
 export default function Home() {
 	const user = useUser<firebase.User>();
 	const router = useRouter();
-	const [show, setShow] = useState(false);
-	const [url, setUrl] = useState('');
-	const { hasCopied, onCopy } = useClipboard(url);
 
 	useEffect(() => {
-		setUrl(`${location.protocol}//${location.host}/api/webhook/github?token=${user?.uid}`);
-
 		if (!user) {
 			router.push('/login');
 		}
 	}, [user]);
 
 	return (
-		<Layout>
+		<>
 			<Head>
 				<title>Home | streamdevs.app</title>
 			</Head>
-			<Center minH="100vh" display="flex">
-				<Stack spacing={3}>
-					<Text>GitHub webhook URL:</Text>
-					<InputGroup>
-						<Input pr="8rem" type={show ? 'text' : 'password'} defaultValue={url} />
-						<InputRightElement width="8rem">
-							<Stack spacing={1} direction="row" justify="right">
-								<Button h="1.75rem" width="3rem" size="sm" onClick={onCopy}>
-									{hasCopied ? 'Copied' : 'Copy'}
-								</Button>
-								<Button h="1.75rem" width="3rem" size="sm" onClick={() => setShow((show) => !show)}>
-									{show ? 'Hide' : 'Show'}
-								</Button>
-							</Stack>
-						</InputRightElement>
-					</InputGroup>
+			<Layout>
+				<Stack spacing="4">
+					<GitHubWebhookCard />
 				</Stack>
-			</Center>
-		</Layout>
+			</Layout>
+		</>
 	);
 }
