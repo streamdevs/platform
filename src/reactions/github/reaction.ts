4afc43b9744f2b6bb11e37894880e378b32a9df4
Santiago Martín Agra <santiagomartinagra@gmail.com>
2020-11-28 17:43:10 +0100

    feat: migrate star reaction for GitHub

---
+++ src/reactions/github/reaction.ts
@@ -0,0 +1,82 @@
+import { StreamLabs } from '../../services/StreamLabs';
+import { TwitchChat } from '../../services/TwitchChat';
+import { WebhookPayload } from './schemas/webhook-payload';
+
+export interface ReactionHandleOptions<P = WebhookPayload> {
+	payload: P;
+}
+
+export interface ReactionCanHandleOptions<P = WebhookPayload> {
+	payload: P;
+	event: string;
+}
+
+export interface ReactionStatus {
+	notified: boolean;
+	message: string;
+}
+
+export abstract class Reaction<P = WebhookPayload> {
+	public constructor(private twitchChat: TwitchChat, private streamlabs: StreamLabs) {}
+
+	abstract getStreamLabsMessage({ payload }: ReactionHandleOptions<P>): string;
+	abstract getTwitchChatMessage({ payload }: ReactionHandleOptions<P>): string;
+	abstract canHandle({ payload, event }: ReactionCanHandleOptions<P>): boolean;
+
+	private async notifyStreamlabs({ payload }: ReactionHandleOptions<P>): Promise<ReactionStatus> {
+		try {
+			const message = this.getStreamLabsMessage({ payload });
+			await this.streamlabs.alert({
+				message,
+			});
+
+			return {
+				notified: true,
+				message,
+			};
+		} catch {
+			// TODO: add logging
+
+			return {
+				notified: false,
+				message: '',
+			};
+		}
+	}
+
+	private async notifyTwitch({ payload }: ReactionHandleOptions<P>): Promise<ReactionStatus> {
+		try {
+			const message = this.getTwitchChatMessage({ payload });
+			await this.twitchChat.send(message);
+
+			return {
+				notified: true,
+				message,
+			};
+		} catch {
+			// TODO: add logging
+
+			return {
+				notified: false,
+				message: '',
+			};
+		}
+	}
+
+	public async handle({
+		payload,
+	}: ReactionHandleOptions<P>): Promise<{
+		streamlabs: ReactionStatus;
+		twitchChat: ReactionStatus;
+	}> {
+		const [streamlabs, twitchChat] = await Promise.all([
+			this.notifyStreamlabs({ payload }),
+			this.notifyTwitch({ payload }),
+		]);
+
+		return {
+			twitchChat,
+			streamlabs,
+		};
+	}
+}
---
+++ tests/reactions/github/star.spec.ts
@@ -0,0 +1,103 @@
+import { StreamLabsMock } from '../../__mocks__/StreamLabs';
+import { TwitchChatMock } from '../../__mocks__/TwitchChat';
+import { StarPayload } from '../../../src/reactions/github/schemas/star-payload';
+import { Star } from '../../../src/reactions/github/star';
+
+describe('Star', () => {
+	let twitchChat: TwitchChatMock;
+	let streamlabs: StreamLabsMock;
+
+	beforeEach(() => {
+		twitchChat = new TwitchChatMock();
+		streamlabs = new StreamLabsMock();
+	});
+
+	describe('#handle', () => {
+		let payload: StarPayload;
+
+		beforeEach(() => {
+			payload = {
+				action: 'created',
+				repository: {
+					full_name: 'streamdevs/webhook',
+					html_url: 'https://github.com/streamdevs/webhook',
+				},
+				sender: {
+					login: 'orestes',
+				},
+			};
+		});
+
+		it("returns 'twitchChat.notified' set to false if something goes wrong in TwitchChat ", async () => {
+			twitchChat.send.mockImplementationOnce(async () => {
+				throw new Error('boom');
+			});
+			const subject = new Star(twitchChat, streamlabs);
+
+			const {
+				twitchChat: { notified },
+			} = await subject.handle({ payload });
+
+			expect(notified).toEqual(false);
+		});
+
+		it("returns 'twitchChat' with the message send to Twitch and notified set to true", async () => {
+			const subject = new Star(twitchChat, streamlabs);
+
+			const { twitchChat: response } = await subject.handle({ payload });
+
+			expect(response).toEqual({
+				message: `${payload.sender.login} just starred ${payload.repository.html_url}`,
+				notified: true,
+			});
+		});
+
+		it("returns 'streamlabs.notified' set to false if something goes wrong with StreamLabs", async () => {
+			streamlabs.alert.mockImplementationOnce(async () => {
+				throw new Error('boooom');
+			});
+			const subject = new Star(twitchChat, streamlabs);
+
+			const {
+				streamlabs: { notified },
+			} = await subject.handle({ payload });
+
+			expect(notified).toEqual(false);
+		});
+
+		it("returns 'streamlabs' with the message send to StreamLabs and notified set to true", async () => {
+			const subject = new Star(twitchChat, streamlabs);
+
+			const { streamlabs: response } = await subject.handle({ payload });
+
+			expect(response).toEqual({
+				notified: true,
+				message: `*${payload.sender.login}* just starred *${payload.repository.full_name}*`,
+			});
+		});
+	});
+
+	describe('#canHandle', () => {
+		it('returns true if the event is star and actions is created', () => {
+			const subject = new Star(twitchChat, streamlabs);
+
+			const result = subject.canHandle({
+				event: 'star',
+				payload: { action: 'created' } as StarPayload,
+			});
+
+			expect(result).toEqual(true);
+		});
+
+		it('returns false if the event is star and actions is removed', () => {
+			const subject = new Star(twitchChat, streamlabs);
+
+			const result = subject.canHandle({
+				event: 'star',
+				payload: { action: 'removed' } as StarPayload,
+			});
+
+			expect(result).toEqual(false);
+		});
+	});
+});
